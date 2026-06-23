91ffaed581a4a7fc735de5090698d3cd4f9efe72
leticialima18 <leticialimadias183@gmail.com>
2026-06-23 09:26:52 -0300

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -1,7 +1,8 @@
 public class Empresa {
     public static void main(String[] args) {
-        Funcionario funcionario1 = new Gerente(nome: "Carlos", salario: 8000);
-        Funcionario funcionario2 = new Desenvolvedor(nome: "Ana", salario: 5000);
+
+        Funcionario funcionario1 = new Gerente("Carlos", 8000);
+        Funcionario funcionario2 = new Desenvolvedor("Ana", 5000);
 
         funcionario1.calcularBonus();
         System.out.println();
@@ -10,39 +11,53 @@ public class Empresa {
     }
 }
 
-    abstract class Funcionario{
+abstract class Funcionario {
 
-        private String nome;
-        private double salario;
+    private String nome;
+    private double salario;
 
-        public Funcionario(String nome, double salario) {
-            this.nome = nome;
-            this.salario = salario;
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
 
-        }
-        public String getNome(){
-            return nome;
-        }
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-        public double getSalario() {
-            return salario;
-        }
-        public void setSalario(double salario){
-            this.salario = salario;
-        }
-        public abstract void calcularBonus();
-} 
+    public String getNome() {
+        return nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public abstract void calcularBonus();
+}
 
 class Gerente extends Funcionario {
 
     public Gerente(String nome, double salario) {
         super(nome, salario);
     }
+
     @Override
-    public void calcularBonus(){
+    public void calcularBonus() {
         double bonus = getSalario() * 0.20;
-        System.out.println(x: "Cargo: Gerente");
+        System.out.println("Cargo: Gerente");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Bônus: " + bonus);
+    }
+}
+
+class Desenvolvedor extends Funcionario {
+
+    public Desenvolvedor(String nome, double salario) {
+        super(nome, salario);
+    }
+
+    @Override
+    public void calcularBonus() {
+        double bonus = getSalario() * 0.10;
+        System.out.println("Cargo: Desenvolvedor");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Bônus: " + bonus);
     }
 }
